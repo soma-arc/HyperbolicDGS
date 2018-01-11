513a2125e47f9596afa9258a2f94ebc6b94bec89
soma_arc <somaarcr@gmail.com>
2018-01-11 20:33:46 +0900

    Consider ideal point

--- src/components/hyperbolicLine.js
+++ src/components/hyperbolicLine.js
@@ -22,9 +22,34 @@ export default class HyperbolicLine extends Circle {
     }
 
     computeCircle() {
-        const c = Circle.fromPoints(this.p1.values,
-                                    this.p2.values,
-                                    Circle.POINCARE_DISK.invertOnPoint(this.p1.values));
+        const p1 = this.p1.values;
+        const p2 = this.p2.values;
+        const p1Inv = Circle.POINCARE_DISK.invertOnPoint(p1);
+        const p2Inv = Circle.POINCARE_DISK.invertOnPoint(p2);
+        const p1OnPoincare = Complex.distance(p1, p1Inv) < 0.00001;
+        const p2OnPoincare = Complex.distance(p2, p2Inv) < 0.00001;
+
+        let c;
+        if (p1OnPoincare && p2OnPoincare) {
+            const center = new Complex((p2.im - p1.im) /
+                                       (p1.re * p2.im - p2.re * p1.im),
+                                       (p1.re - p2.re) /
+                                       (p1.re * p2.im - p2.re * p1.im));
+            c = new Circle(center.re, center.im,
+                           Complex.distance(center, p1));
+        } else if (p1OnPoincare) {
+            c = Circle.fromPoints(p1,
+                                  p2,
+                                  p2Inv);
+        } else if (p2OnPoincare) {
+            c = Circle.fromPoints(p1,
+                                  p2,
+                                  p1Inv);
+        } else {
+            c = Circle.fromPoints(p1,
+                                  p2,
+                                  p1Inv);
+        }
         this.center = c.center;
         this.r = c.r;
     }
